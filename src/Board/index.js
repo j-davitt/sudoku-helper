20752f03f1067f703622bbf4b79bc3c806f6c347
Joseph Davitt <joseph.j.davitt@gmail.com>
2024-02-05 13:57:29 -0800

    create number input

--- src/Board/index.js
+++ src/Board/index.js
@@ -2,6 +2,19 @@ import React from "react";
 
 const Board = () => {
 
+  let boardData = {};
+
+  // let blocks = [];
+  // const fillBlocks = () => {
+  //   for(let i = 0; i < 9; i++){
+  //     blocks.push({
+  //       value: i,
+  //     })
+  //   }
+  // };
+
+  // fillBlocks();
+
   let blocks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let groups = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -12,7 +25,10 @@ const Board = () => {
         {groups.map((group, i) => (
           <div key={`group-${i}`} className="board-group">
             {blocks.map((block, j) => (
-              <div key={`block-${i}-${j}`} className="board-block">{`${i}-${j}`}</div>
+              <div key={`block-${i}-${j}`} className="board-block">
+                {/* {`${i}-${j}`} */}
+                <input className='number-input' type="number" min={1} max={9}></input>
+                </div>
             ))}
           </div>
         ))}
